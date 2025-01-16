a0dc2565383c2231e282e1f438860daf279a4111
AdsonTanajura <adson.tanajura@gmail.com>
2025-01-16 16:29:02 -0300

    Desafio 02

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 // Desavio 01
 
-let INDICE = 13;
-let SOMA = 0;
-let K = 0;
+// let INDICE = 13;
+// let SOMA = 0;
+// let K = 0;
 
-while (K < INDICE) {
-    K++
-    SOMA = SOMA + K
-}
+// while (K < INDICE) {
+//     K++
+//     SOMA = SOMA + K
+// }
 
-console.log(SOMA)
+// console.log(SOMA)
+
+
+// Desafio 02
+
+// const isFibonacci = (number) => {
+//     if (number < 0) return false;
+
+//     let a = 0;
+//     let b = 1;
+
+//     if (number === a || number === b) return true;
+
+//     let next = a + b;
+
+//     while (next <= number) {
+//         if (next === number) return true;
+//         a = b;
+//         b = next;
+//         next = a + b;
+//     }
+
+//     return false;
+// }
+
+// const numero = 21;
+// if (isFibonacci(numero)) {
+//     console.log(`O número ${numero} pertence à sequência de Fibonacci.`);
+// } else {
+//     console.log(`O número ${numero} NÃO pertence à sequência de Fibonacci.`);
+// }
+
+
